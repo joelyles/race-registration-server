ca4e642ed3ccd0bdd38c86befe37c875a142aafe
Joe Lyles <lylesjoe@protonmail.com>
2024-05-08 06:18:38 -0400

    connected to db

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dbConnect = require('./config/dbConnection.js');
 const PORT = process.env.PORT || 5000;
+
+dbConnect();
 
 app.use(express.json());
 
@@ -12,4 +16,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>');
 });
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+mongoose.connection.once('open', () => {
+    console.log('this app is connected to MongoDB')
+    app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+});
